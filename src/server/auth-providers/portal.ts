43801fc05e11275335d5f9be9c0d7d701c0f4ba9
Dominik Vít <slipiklp@gmail.com>
2024-01-08 00:13:48 +0100

    fix: removed localhost from portal provider

--- src/server/auth-providers/portal.ts
+++ src/server/auth-providers/portal.ts
@@ -17,9 +17,10 @@ export default function PortalProvider<P extends PortalProfile>(
     id: "portal",
     name: "The Space Portal",
     type: "oauth",
-    authorization: "http://localhost:4000/oauth2/v1/authorize?scope=auth",
-    token: "http://localhost:4000/oauth2/v1/token",
-    userinfo: "http://localhost:4000/oauth2/v1/userinfo",
+    authorization:
+      "https://portal.thepsace.company/oauth2/v1/authorize?scope=auth",
+    token: "https://portal.thepsace.company/oauth2/v1/token",
+    userinfo: "https://portal.thepsace.company/oauth2/v1/userinfo",
     profile(profile) {
       return {
         id: profile.id,
@@ -38,7 +39,7 @@ export default function PortalProvider<P extends PortalProfile>(
 //     id: "portal",
 //     name: "The Space Portal",
 //     type: "oauth",
-//     wellKnown: "http://localhost:4000/.well-known/openid-configuration",
+//     wellKnown: "https://portal.thepsace.company/.well-known/openid-configuration",
 //     idToken: true,
 //     profile(profile) {
 //       return {
